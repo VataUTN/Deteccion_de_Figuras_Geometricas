a715cd7c0886d6cf5643314b88056cb0902b8b5f
francosantzz <129998263+francosantzz@users.noreply.github.com>
2023-05-20 19:15:26 -0300

    Agrego clase Entrenador

---
+++ Codigo/Entrenador.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from pathlib import Path
+
+
+class Entrenador:
+    def __init__(self, modelo, imagenes_entrenamiento, etiquetas_entrenamiento):
+        self.modelo = modelo
+        self.imagenes_entrenamiento = imagenes_entrenamiento
+        self.etiquetas_entrenamiento = etiquetas_entrenamiento
+
+    def entrenar(self):
+        ruta_modelo = Path('ModeloEntrenado.h5')
+        # Comprobar si el archivo que contiene el modelo entrenado existe.
+        if ruta_modelo.is_file():
+            # Cargar el modelo entrenado:
+            modelo_entrenado = tf.keras.models.load_model(str(ruta_modelo))
+            print('Modelo existente cargado desde archivo.')
+            return modelo_entrenado
+        else:
+            # Si el archivo no existe, se entrena el modelo y se guarda:
+            self.modelo.arquitectura.fit(
+                self.imagenes_entrenamiento,
+                self.etiquetas_entrenamiento,
+                epochs=self.modelo.epocas,
+                batch_size=self.modelo.bache,
+                verbose=True)
+
+            # Guardar el modelo entrenado:
+            self.modelo.arquitectura.save('ModeloEntrenado.h5')
+            print('Modelo entrenado y guardado en archivo.')
