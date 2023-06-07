2d7b4f83850c715f0c51381f1dbae9a9b7cd899f
lucascardone3 <129989551+lucascardone3@users.noreply.github.com>
2023-06-07 11:51:19 -0300

    Eliminando directorio de generación de datastet

--- Codigo/generador_dataset/Generador_Dataset.py
+++
@@ -1,8 +0,0 @@
-from generador_dataset.generador import figurasGeometricas
-
-
-def generarFiguras(tamanio, destino, animacion):
-    generador = figurasGeometricas(
-        tamanio=tamanio, destino=destino, animacion=animacion
-    )
-    generador.generar()
