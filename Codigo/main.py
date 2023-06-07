5b7c69362a9270688d42b220080cb882e35b0f77
LucaZampe <129890529+LucaZampe@users.noreply.github.com>
2023-06-07 12:21:43 -0300

    Actualizo Main
    Se redujeron considerablemente la cantidad de líneas ya que la instalación de las librerías necesarias se encuentra distribuida en métodos del Instalador.

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,31 +1,12 @@
-import os
-import subprocess
-import sys
-from UI import PaginaPrincipal as pag
+from Codigo import Instalador
 
-
-def crear_directorios():
-    if not os.path.exists("imagen_a_analizar"):
-        os.makedirs("imagen_a_analizar")
-    if not os.path.exists("imagenes_normalizadas"):
-        os.makedirs("imagenes_normalizadas")
-    if not os.path.exists("imagen_analizada"):
-        os.makedirs("imagen_analizada")
-
-def instalar_dependencias():
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    except subprocess.CalledProcessError as e:
-        print("Error al instalar las dependencias:")
-        print(e)
-        sys.exit(1)
-
+# Clase Principal.
 if __name__ == "__main__":
-    # En caso de no existir, se crean los directorios necesarios.
-    crear_directorios()
+    # Instalamos las dependencias necesarias (linux):
+    Instalador.instalar_dependencias()
 
-    # Instalamos las dependencias necesarias:
-    # instalar_dependencias()
+    # Importar el módulo PaginaPrincipal:
+    import PaginaPrincipal as pag
 
     # Iniciar la UI:
     app = pag.App()
